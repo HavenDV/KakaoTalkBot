f33ec1d6c28d095d8f98b958a7dbd073bea66f6c
HavenDV <havendv@gmail.com>
2018-03-12 17:58:01 +1000

    Fixed tests

--- KakaoTalkBotLibrary.Tests/TestClass.cs
+++ KakaoTalkBotLibrary.Tests/TestClass.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using Emgu.CV;
+using KakaoTalkBotLibrary.Extensions;
 using KakaoTalkBotLibrary.Tests.Utilities;
 using KakaoTalkBotLibrary.Utilities;
 using NUnit.Framework;
@@ -14,7 +16,7 @@ namespace KakaoTalkBotLibrary.Tests
     {
         #region Properties
 
-        private static Dictionary<string, Mat> Anchors { get; } = MatUtilities.LoadImages(TestUtilities.AnchorsDirectory);
+        private static Screens Screens { get; } = new Screens(TestUtilities.AnchorsDirectory);
         private static Dictionary<string, Mat> Screenshots { get; } = MatUtilities.LoadImages(TestUtilities.ScreenshotsDirectory);
 
         #endregion
@@ -24,19 +26,25 @@ namespace KakaoTalkBotLibrary.Tests
         [Test]
         public void TestMethod()
         {
-            IsExistsBaseTest("find_menu.bmp", "find");
+            IsExistsBaseTest("add_by_contacts_max.bmp", "Back", "add_by_contacts");
         }
 
         #endregion
 
         #region Base Tests
 
-        private static void IsExistsBaseTest(string anchorName, string prefix)
+        private static void IsExistsBaseTest(string screenName, string anchorName, string prefix)
         {
-            var anchor = GetAnchor(anchorName, Anchors);
+            var (screen, anchor) = GetAnchor(anchorName, screenName, Screens);
             foreach (var pair in GetImagesWithPrefix(prefix, Screenshots))
             {
-                var result = MatUtilities.IsExistsEx(pair.Value, anchor);
+                var anchorMat = new Mat(screen.Mat, anchor.Rectangle);
+
+                var fx = 1.0 * pair.Value.Width / screen.Mat.Width;
+                var fy = 1.0 * pair.Value.Height / screen.Mat.Height;
+                var resizedAnchorMat = anchorMat.Resize(Size.Empty, fx, fy);
+
+                var result = MatUtilities.IsExistsEx(pair.Value, resizedAnchorMat);
                 if (!result.isExists)
                 {
                     Assert.Warn($"!MatUtilities.IsExists: Anchor: {anchorName}. Image: {pair.Key}. Threshold: {result.threshold}. Edge threshold: {MatUtilities.IsExistsThreshold}");
@@ -48,10 +56,17 @@ namespace KakaoTalkBotLibrary.Tests
 
         #region Utilities
 
-        private static Mat GetAnchor(string name, IReadOnlyDictionary<string, Mat> dictionary) =>
-            dictionary.TryGetValue(name, out var result)
-                ? result
-                : throw new FileNotFoundException($"Anchor is not found: {name}");
+        private static (Screen screen, Anchor anchor) GetAnchor(string name, string screenName, Screens screens)
+        {
+            var screen = screens.FirstOrDefault(i => string.Equals(i.Name, screenName, StringComparison.OrdinalIgnoreCase));
+            var anchor = screen
+                             ?.Anchors
+                             ?.FirstOrDefault(i => string.Equals(i.Name, name, StringComparison.OrdinalIgnoreCase)) ??
+                             throw new FileNotFoundException($"Anchor is not found: {name}");
+
+
+            return (screen, anchor);
+        }
 
         private static Dictionary<string, Mat> GetImagesWithPrefix(string prefix, Dictionary<string, Mat> dictionary) =>
             dictionary
